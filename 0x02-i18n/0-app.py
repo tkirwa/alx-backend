d54fe29e462bad2b4fe7ff2b570647490a5eac27
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-02 14:00:55 +0300

    0-app.py, templates/0-index.html

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-
 """ Basic Flask app """
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """ index page """
-    return render_template(0-index.html)
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """Main route"""
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
